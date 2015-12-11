c4a0863cb6810fd9079ba3bdcd7444439fb0efff
Léo-Paul COUTURIER <leopaul3@gmail.com>
2015-12-11 17:46:12 +0100

    Refactor du nom des controlleurs

--- src/com/traveloid/trippple/controllers/LogoutController.java
+++ src/com/traveloid/trippple/controllers/LogoutController.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpSession;
  * Servlet implementation class LogoutServlet
  */
 @WebServlet("/logout")
-public class LogoutServlet extends HttpServlet {
+public class LogoutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
