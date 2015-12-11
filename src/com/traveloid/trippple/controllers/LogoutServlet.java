6617a2cf9c0b2b093a6af11ec6661f54751c8169
Léo-Paul COUTURIER <leopaul3@gmail.com>
2015-12-11 17:10:32 +0100

    Ajout de LogoutServlet

---
+++ src/com/traveloid/trippple/controllers/LogoutServlet.java
@@ -0,0 +1,30 @@
+package com.traveloid.trippple.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.removeAttribute("id");
+
+		response.sendRedirect(request.getContextPath());
+	}
+
+}
--- src/com/traveloid/trippple/entity/Trip.java
+++ src/com/traveloid/trippple/entity/Trip.java
@@ -9,30 +9,29 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.xml.bind.annotation.XmlRootElement;
 
 import org.json.simple.JSONAware;
 import org.json.simple.JSONObject;
 
 @Entity
-@Table(name="Trips")
+@Table(name = "Trips")
 public class Trip implements Serializable, JSONAware {
 
 	/**
 	 * Necessaire pour Serializable
 	 */
 	private static final long serialVersionUID = -2295692331115445380L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	@ManyToOne
-    @JoinColumn(name="origin_id")
+	@JoinColumn(name = "origin_id")
 	private Campus origin;
-	
+
 	@ManyToOne
-    @JoinColumn(name="destination_id")
+	@JoinColumn(name = "destination_id")
 	private Campus destination;
 
 	public long getId() {
@@ -60,9 +59,12 @@ public class Trip implements Serializable, JSONAware {
 	}
 
 	@Override
+	@SuppressWarnings({
+			"boxing", "unchecked"
+	})
 	public String toJSONString() {
 		JSONObject obj = new JSONObject();
-		
+
 		obj.put("id", this.id);
 		obj.put("origin", this.origin);
 		obj.put("destination", this.destination);
