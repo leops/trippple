184fcc5e5aa4a3d459345476b84dff8064ace152
Léo-Paul COUTURIER <leopaul3@gmail.com>
2015-12-13 20:56:23 +0100

    Ajout de le gestion des messages flash dans le GlobalFilter

--- src/com/traveloid/trippple/filter/GlobalFilter.java
+++ src/com/traveloid/trippple/filter/GlobalFilter.java
@@ -10,14 +10,17 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import com.traveloid.trippple.dao.UserDao;
 import com.traveloid.trippple.dao.jpa.JpaUserDao;
 
-@WebFilter(urlPatterns = {"/*"})
+@WebFilter(urlPatterns = {
+		"/*"
+})
 public class GlobalFilter implements Filter {
 	private UserDao dao = null;
-	
+
 	@Override
 	public void init(FilterConfig config) throws ServletException {
 		this.dao = new JpaUserDao();
@@ -27,9 +30,17 @@ public class GlobalFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
 		if(request != null) {
-			Long id = (Long) request.getSession().getAttribute("user");
+			HttpSession session = request.getSession();
+
+			Long id = (Long) session.getAttribute("user");
 			if(id != null) {
 				req.setAttribute("user", this.dao.findById(id));
+			}
+
+			String flash = (String) session.getAttribute("flash");
+			if(flash != null) {
+				req.setAttribute("flash", flash);
+				session.setAttribute("flash", null);
 			}
 		}
 		chain.doFilter(req, res);
