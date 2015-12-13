ee75cbb4a5622757b06b5c433753be5374296217
Léo-Paul COUTURIER <leopaul3@gmail.com>
2015-12-13 20:57:01 +0100

    Fix de l'ajout de trip au bag

--- src/com/traveloid/trippple/controllers/AddTripControler.java
+++ src/com/traveloid/trippple/controllers/AddTripControler.java
@@ -2,7 +2,6 @@ package com.traveloid.trippple.controllers;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,46 +20,29 @@ public class AddTripControler extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public AddTripControler() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		RequestDispatcher rd = request.getRequestDispatcher("/auth/addTrip.jsp");
-		rd.forward(request, response);
-
-	}
-
-	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		Long tripId = Long.parseLong((request.getParameter("id")));
-
-		JpaTripDao tripDao = new JpaTripDao();
-		Trip trip = tripDao.findById(tripId);
-
-		if(trip == null) {
-			response.sendRedirect(request.getContextPath());
-		}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String home = request.getContextPath();
 
 		User user = (User) request.getAttribute("user");
 		if(user == null) {
-			response.sendRedirect(request.getContextPath() + "/login");
+			response.sendRedirect(home);
+			return;
+		}
+
+		long tripId = Long.parseLong(request.getParameter("id"));
+
+		JpaTripDao tripDao = new JpaTripDao();
+		Trip trip = tripDao.findById(tripId);
+		if(trip == null) {
+			response.sendRedirect(home);
 			return;
 		}
 
 		user.getBag().add(trip);
 
+		response.sendRedirect(home);
 	}
 }
