deeb98a8d0ad820421c822962823b9b390bef89b
Léo-Paul COUTURIER <leopaul3@gmail.com>
2015-12-13 20:57:21 +0100

    Fix du logout

--- src/com/traveloid/trippple/controllers/LogoutController.java
+++ src/com/traveloid/trippple/controllers/LogoutController.java
@@ -22,7 +22,7 @@ public class LogoutController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		session.removeAttribute("id");
+		session.removeAttribute("user");
 
 		response.sendRedirect(request.getContextPath());
 	}
